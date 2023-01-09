52f97aa16ee3e3b2ef3a8dde7975c40521a8d7de
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-09 15:58:53 -0800

    Added explanatory to the window alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-window.alert(subtract(number1, number2));
-window.alert(multiply(number1, number2));
-window.alert(divide(number1, number2));
+window.alert("The addition of your numbers equals " + add(number1, number2) + ".");
+window.alert("The subtraction of your second number from your first number equals " + subtract(number1, number2) + ".");
+window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".");
+window.alert("The division of your second number by your first number equals " + divide(number1, number2) + ".");
