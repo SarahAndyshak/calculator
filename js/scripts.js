1bbc52555ed9924d6620f8ec3b3e373f4a317f40
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-09 15:51:16 -0800

    Added multiply and divide functions and window.alerts to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,19 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number2 / number1;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
 window.alert(subtract(number1, number2));
+window.alert(multiply(number1, number2));
+window.alert(divide(number1, number2));
