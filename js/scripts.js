f510d3d953384194c20870910ff3a2f41addcb12
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-09 15:45:34 -0800

    Added subtract function and window.alert subtract function to scripts.jss.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 // user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
+window.alert(subtract(number1, number2));
