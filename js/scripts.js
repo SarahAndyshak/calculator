e1ee8896e4f24cf2b1d0595362e075d234e9de14
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-09 15:39:47 -0800

    Refactored code and added README.md.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+// business logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
+// user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-const result = add(3, 5);
-window.alert(result);
+window.alert(add(number1, number2));
