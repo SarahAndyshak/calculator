c20839ee4c2a9f467bd11be3e2097a70fac27aaf
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-09 16:15:05 -0800

    Created one long window.alert, had to add several sub-parentheses to separate alert segments.

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals " + add(number1, number2) + ".");
-window.alert("The subtraction of your second number from your first number equals " + subtract(number1, number2) + ".");
-window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".");
-window.alert("The division of your second number by your first number equals " + divide(number1, number2) + ".");
+//window.alert("The addition of your numbers equals " + add(number1, number2) + ".");
+//window.alert("The subtraction of your second number from your first number equals " + subtract(number1, number2) + ".");
+//window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".");
+//window.alert("The division of your second number by your first number equals " + divide(number1, number2) + ".");
+
+window.alert(("Addition result " + add(number1,number2) + ", ") + ("Subtraction result " + subtract(number1, number2) + ", ") + ("Multiplication result " + multiply(number1, number2) + ", ") + ("Division result " + divide(number1, number2)));
